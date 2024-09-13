a13042e6a774116bdee15fb160fdc483645e7666
valcuiwu <2973712698@qq.com>
2024-09-13 21:33:50 +0800

    first koa

---
+++ index.js
@@ -0,0 +1,11 @@
+import Koa from 'koa'
+
+const server = new Koa()
+
+server.use((ctx)=>{
+    ctx.body='hello world'
+})
+
+server.listen(8080,'0.0.0.0',()=>{
+    console.log("Server is listening on http://127.0.0.1:8080")
+})
