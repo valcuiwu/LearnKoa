ed77579270102111380d24ed2cc35f3fc0a15635
valcuiwu <2973712698@qq.com>
2024-09-17 22:45:57 +0800

    添加router，未封装

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-import Koa from 'koa'
+import Koa from "koa";
+import KoaRouter from "koa-router";
 
 //1. server即为文档中的application对象
-const server = new Koa()
+const server = new Koa();
+const router = new KoaRouter();
+//注册路由表
+router.get("/", (ctx) => {
+  ctx.body = "hello world";
+});
+
+//字段是post/get，前端请求就要用post/get方法来发送
+//如果方法用错，会报405（方法不允许）
+router.post("/login", (ctx) => {
+    ctx.body = "login";
+  });
 
 
 //一· 验证用户是否合法
-server.use((/* 2.ctx即为文档中context对象 */ctx,next)=>{
-    //3.ctx.request 即为文档中的request对象
-    //4.ctx.response即为文档中的response对象
-    console.log(1)
-    next()
-    console.log(11)
-})
+server.use(async (/* 2.ctx即为文档中context对象 */ ctx, next) => {
+  //3.ctx.request 即为文档中的request对象
+  //4.ctx.response即为文档中的response对象
+  console.log(1);
+  if (true) {
+    await next();
+  } else {
+    ctx.body = "失败";
+  }
+  console.log(11);
+});
 
 //二·处理业务逻辑
-server.use((ctx,next)=>{
-    console.log(2)
-    next()
-    console.log(22)
-})
+server.use(router.routes())
+//响应时会给头部增加一个allowedMethods字段
+.use(router.allowedMethods());
 
-server.use((ctx,next)=>{
-    console.log(3)
-    next()
-    console.log(33)
-})
 
-server.use((ctx,next)=>{
-    console.log(4)
-    next()
-    console.log(44)
-})
-
-server.listen(8080,'0.0.0.0',()=>{
-    console.log("Server is listening on http://127.0.0.1:8080")
-})
+server.listen(8080, "0.0.0.0", () => {
+  console.log("Server is listening on http://127.0.0.1:8080");
+});
